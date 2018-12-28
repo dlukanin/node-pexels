501bee81116efb526c08127faf63da23520eef83
Danilo Gómez Gómez <danilogomez3.14@gmail.com>
2018-12-28 14:35:25 -0500

    added `photos` to unit test

--- src/test/unit/main.ts
+++ src/test/unit/main.ts
@@ -4,6 +4,17 @@ import {expect} from 'chai';
 const handleThen = (done) => (result) => done('expected error, instead got ' + result);
 
 describe('pexels client', function(): void {
+    it('should throw error after passing invalid photo param', function(done: MochaDone): void {
+        const client = new DefaultPexelsClient('test');
+        const param = '0';
+        (client as any).photo(param)
+            .then(handleThen(done))
+            .catch((error) => {
+                expect(error.message).to.eq('Pexels client: invalid id param: ' + param);
+                done();
+            });
+    });
+
     it('should throw error after passing invalid search params', function(done: MochaDone): void {
         const client = new DefaultPexelsClient('test');
         (client as any).search('forest', '10', null)
