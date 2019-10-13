0ff5fb7fe5730eb063787020074a69deaf891c9f
Paulius And <ekstras@gmail.com>
2019-10-13 23:41:39 +0300

    Migrate to jest (#6)
    * add jest, remove mocha/chai
    
    * migrate tests to jest
    
    * add verbose to jest config for more output
    
    * set tests folder for jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  roots: [
+    '<rootDir>/src/test/'
+  ],
+  testEnvironment: 'node',
+  verbose: true
+};
--- src/test/integration/main.test.ts
+++ src/test/integration/main.test.ts
@@ -1,15 +1,14 @@
 import {DefaultPexelsClient} from '../../lib/client/default';
 import {config} from '../config';
-import * as chai from 'chai';
-import * as jsonSchema from 'chai-json-schema';
+import {matchers} from 'jest-json-schema';
 import {fetchSchema, photoSchema, responseSchema} from '../response_schema';
 
-chai.use(jsonSchema);
+expect.extend(matchers);
 
-describe('pexels client', function(): void {
-    this.timeout(20000);
+describe('pexels client integration tests', (): void => {
+    jest.setTimeout(20000);
 
-    it('should throw error on photo method with invalid api key', function(done: MochaDone): void {
+    it('should throw error on photo method with invalid api key', (done: jest.DoneCallback): void => {
         const client = new DefaultPexelsClient('test');
         client.photo(0)
             .then((result) => {
@@ -20,7 +19,7 @@ describe('pexels client', function(): void {
             });
     });
 
-    it('should throw error on search method with invalid api key', function(done: MochaDone): void {
+    it('should throw error on search method with invalid api key', (done: jest.DoneCallback): void => {
         const client = new DefaultPexelsClient('test');
         client.search('forest', 10, 2)
             .then((result) => {
@@ -31,7 +30,7 @@ describe('pexels client', function(): void {
             });
     });
 
-    it('should throw error on popular method with invalid api key', function(done: MochaDone): void {
+    it('should throw error on popular method with invalid api key', (done: jest.DoneCallback): void => {
         const client = new DefaultPexelsClient('test');
         client.popular(10, 2)
             .then((result) => {
@@ -42,23 +41,23 @@ describe('pexels client', function(): void {
             });
     });
 
-    it('should throw error on photo method with 404 status code', function(done: MochaDone): void {
+    it('should throw error on photo method with 404 status code', (done: jest.DoneCallback): void => {
         const client = new DefaultPexelsClient(config.apiKey);
         client.photo(0)
             .then((result) => {
                 done('expected error, instead got ' + result);
             })
             .catch((error) => {
-                chai.expect(error.message).match(/.*404.*/);
+                expect(error.message).toMatch(/.*404.*/);
                 done();
             });
     });
 
-    it('should return valid response on photo method call', function(done: MochaDone): void {
+    it('should return valid response on photo method call', (done: jest.DoneCallback): void => {
         const client = new DefaultPexelsClient(config.apiKey);
         client.photo(1261427)
             .then((result) => {
-                chai.expect(result).to.be.jsonSchema(photoSchema);
+                expect(result).toMatchSchema(photoSchema);
                 done();
             })
             .catch((error) => {
@@ -66,11 +65,11 @@ describe('pexels client', function(): void {
             });
     });
 
-    it('should return valid response on search method call', function(done: MochaDone): void {
+    it('should return valid response on search method call', (done: jest.DoneCallback): void => {
         const client = new DefaultPexelsClient(config.apiKey);
         client.search('people', 5, 1)
             .then((result) => {
-                chai.expect(result).to.be.jsonSchema(responseSchema);
+                expect(result).toMatchSchema(responseSchema);
                 done();
             })
             .catch((error) => {
@@ -78,11 +77,11 @@ describe('pexels client', function(): void {
             });
     });
 
-    it('should return valid response on popular method call', function(done: MochaDone): void {
+    it('should return valid response on popular method call', (done: jest.DoneCallback): void => {
         const client = new DefaultPexelsClient(config.apiKey);
         client.popular(5, 1)
             .then((result) => {
-                chai.expect(result).to.be.jsonSchema(responseSchema);
+                expect(result).toMatchSchema(responseSchema);
                 done();
             })
             .catch((error) => {
@@ -90,15 +89,15 @@ describe('pexels client', function(): void {
             });
     });
 
-    it('should fetch photo', function(done: MochaDone): void {
+    it('should fetch photo', (done: jest.DoneCallback): void => {
         const client = new DefaultPexelsClient(config.apiKey);
         client.popular(5, 1)
             .then((result) => {
                 return client.fetch(result.photos[1], 'small');
             })
             .then((result) => {
-                chai.expect(result).to.be.jsonSchema(fetchSchema);
-                chai.expect(result.data).to.be.instanceOf(Buffer);
+                expect(result).toMatchSchema(fetchSchema);
+                expect(result.data).toBeInstanceOf(Buffer);
 
                 done();
             })
